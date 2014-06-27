2ce10f50e53254678c71c561b83475512d094f33
jeremyscully <jeremys@expedia.com>
2014-06-27 10:29:25 -0700

    Adding Regression PriZero Call

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) { 
       System.out.println("Hello, World");
       System.out.println("Continuous Integration Test");
+      System.out.println("Adding Regression PriZero Call");
    }
 }
